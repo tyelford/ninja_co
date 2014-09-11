c702b1a7e1e6386d2bbe5c731184c25a9560fc1a
tyelford <tyelford@gmail.com>
2014-09-11 19:13:33 +1000

    First Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  
+  <head>
+    
+    <!-- Style Sheets -->
+    <link href='css/main.css' rel='stylesheet' />
+    
+    <!-- Google Web Font - Open Sans -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+    
+  </head>
+  
+  <body>
+    
+    <div id='logo'>
+        
+      </div>
+    
+    <div id='header'>
+      <ul>
+        <li>Contact</li>
+        <li>The Builders</li>
+        <li>Our Work</li>
+        <li>News</li>
+      </ul>
+    </div>
+    
+  </body>
+  
+</html>
